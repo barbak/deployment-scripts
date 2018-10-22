85237b8736063273d4ae8a73307bb926d83aa852
Alexandre Parent <alexandre.parent@dont-nod.com>
2018-10-22 14:42:15 +0200

    msys2 uninstall: Can remove the installer with the appropriate option.

--- dne-msys2-uninstall.ps1
+++ dne-msys2-uninstall.ps1
@@ -8,6 +8,9 @@
 .PARAMETER deployArea
     Specify where the MSYS2 directory is.
 
+.PARAMETER removeInstaller
+    Specify if installer should be removed from disk.
+
 .NOTES
     Originally, the script was intended to be used by Patoune.
 #>
@@ -15,16 +18,28 @@
 param(
     [Parameter(Mandatory=$true)]
     [Alias("da")]
-    [string]$deployArea
+    [string]$deployArea,
+    [Alias("ri")]
+    [bool]$removeInstaller=$false
 )
 
+$msys2Installer="$deployArea/msys2-base-x86_64-20180531.tar.xz";
+
 if (Test-Path "$deployArea\msys64") {
-    Write-Host -NoNewline "Removing $deployArea\msys64 ..."
-    Remove-Item -Recurse -Force $deployArea\msys64
-    Write-Host " Done."
-} 
-else {
-    Write-Host "Nothing to remove in $deployArea."
+    Write-Host -NoNewline "Removing '$deployArea\msys64' ...";
+    Remove-Item -Recurse -Force $deployArea\msys64;
+    Write-Host " Done.";
+} else {
+    Write-Host "Nothing to remove in $deployArea.";
 }
 
-Start-Sleep 3
+if ($removeInstaller -eq $true) {
+    Write-Host "Removing installers ...";
+    if (Test-Path $msys2Installer) {
+        Remove-Item -Force $msys2Installer;
+        Write-Host "  '$msys2Installer' removed.";
+    }
+    Write-host "Done.";
+}
+
+Start-Sleep 3;
